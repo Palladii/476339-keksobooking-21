2d99388308c1d16fb4992d824b0a9097bfbe2ec1
Palladii <opalladii@gmail.com>
2020-10-14 10:49:18 +0300

    Deleted unnecessary files & replaced var, quotes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const TITLE = ['Уютное место', 'Отдых у моря', 'Для веселой компании', 'Семейная идилия', 'То что надо', 'Дом в облаках', 'Спортивное место', 'Романтическое место'];
+const TITLE = [`Уютное место`, `Отдых у моря`, `Для веселой компании`, `Семейная идилия`, `То что надо`, `Дом в облаках`, `Спортивное место`, `Романтическое место`];
 const PRICE = [1000, 1500, 2000, 2500, 3500, 4000, 4100, 10000];
-const TYPE = ['palace', 'flat', 'house', 'bungalow'];
+const TYPE = [`palace`, `flat`, `house`, `bungalow`];
 const ROOMS = [1, 2, 3, 4, 5];
 const GUESTS = [1, 2, 3, 4];
-const CHECKIN_CHECKOUT = ['12:00', '13:00', '14:00'];
-const FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-const DESCRIPTION = ['Лучшее предложение', 'Лучшая цена', 'Высокая оценка гостей', 'Рядом с центром'];
-const PHOTOS_ROOMS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const CHECKIN_CHECKOUT = [`12:00`, `13:00`, `14:00`];
+const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const DESCRIPTION = [`Лучшее предложение`, `Лучшая цена`, `Высокая оценка гостей`, `Рядом с центром`];
+const PHOTOS_ROOMS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 const minMapX = 40;
 const maxMapX = 1160;
 const minMapY = 130;
@@ -16,11 +16,11 @@ const maxMapY = 630;
 const minOffer = 1;
 const maxOffer = 8;
 
-const map = document.querySelector('.map');
-map.classList.remove('map--faded');
+const map = document.querySelector(`.map`);
+map.classList.remove(`map--faded`);
 
-const mapPins = document.querySelector('.map__pins');
-const buttonTemplate = document.querySelector('#pin').content.querySelector('button');
+const mapPins = document.querySelector(`.map__pins`);
+const buttonTemplate = document.querySelector(`#pin`).content.querySelector(`button`);
 
 const getRandomInteger = function (min, max) {
   let rand = min + Math.random() * (max + 1 - min);
@@ -35,11 +35,11 @@ const getRandomElement = function (items) {
 const getNewCard = function () {
   return {
     author: {
-      avatar: "img/avatars/user0" + getRandomInteger(minOffer, maxOffer) + ".png",
+      avatar: `img/avatars/user` + 0 + getRandomInteger(minOffer, maxOffer) + `.png`,
     },
     offer: {
       title: getRandomElement(TITLE),
-      address: 'location.x, location.y',
+      address: `location.x, location.y`,
       price: getRandomElement(PRICE),
       type: getRandomElement(TYPE),
       rooms: getRandomElement(ROOMS),
@@ -58,16 +58,16 @@ const getNewCard = function () {
 };
 
 
-var renderButton = function (cardData) {
-  var buttonPin = buttonTemplate.cloneNode(true);
+const renderButton = function (cardData) {
+  let buttonPin = buttonTemplate.cloneNode(true);
   buttonPin.style.left = `${cardData.location.x}px`;
   buttonPin.style.top = `${cardData.location.y}px`;
-  buttonPin.querySelector('img').src = cardData.author.avatar;
+  buttonPin.querySelector(`img`).src = cardData.author.avatar;
   return buttonPin;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < maxOffer; i++) {
+const fragment = document.createDocumentFragment();
+for (let i = 0; i < maxOffer; i++) {
   fragment.appendChild(renderButton(getNewCard()));
 }
 
